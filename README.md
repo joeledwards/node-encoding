e44448b2c9d72f942b1e409ae2a125153593c14e
Joel Edwards <joeledwards@gmail.com>
2018-07-07 08:00:45 -0700

    feat: created b64enc and b64enc utilities

---
+++ README.md
@@ -0,0 +1,11 @@
+# encoding
+
+CLI utilities for (en/de)coding common formats.
+
+## Utils
+
+- `b64enc` - base64 encode a stream of bytes
+- `b64dec` - decode a base64-encoded stream
+- `hexenc` - hex encode a stream of bytes
+- `hexdec` - decode a hex-encoded stream
+
---
+++ bin/b64dec.js
@@ -0,0 +1,36 @@
+#!/usr/bin/env node
+
+let buffers = []
+let totalLength = 0
+
+let total = 0
+
+// Process each chunk of data as it is received
+process.stdin.on('data', data => {
+  buffers.push(data)
+  totalLength += data.byteLength
+  total += data.byteLength
+
+  if (totalLength >= 8192) {
+    const writeBuffer = Buffer.concat(buffers)
+    totalLength = writeBuffer.byteLength % 4
+    const writeCount = writeBuffer.byteLength - totalLength
+    process.stdout.write(Buffer.from(writeBuffer.slice(0, writeCount).toString(), 'base64'))
+
+    buffers = []
+    if (totalLength > 0) {
+      const remainder = writeBuffer.slice(writeCount)
+      buffers.push(remainder)
+    }
+  }
+})
+
+// Flush any remaining data
+process.stdin.on('end', () => {
+  if (buffers.length > 0) {
+    const writeBuffer = Buffer.concat(buffers)
+    process.stdout.write(Buffer.from(writeBuffer.toString(), 'base64'))
+  }
+})
+
+process.stdin.resume()
